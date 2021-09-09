e10d23f3f821ab8f9c26b6222e7bc8eafd046a49
abhishekghule18 <abhishekghule018@gmail.com>
2021-09-09 19:27:32 +0530

    level 2

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "thisisourlittleSecret" ;
-userSchema.plugin(encrypt , {secret: secret ,  encryptedFields: ["password"] });
+userSchema.plugin(encrypt , {secret: process.env.SECRET ,  encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
